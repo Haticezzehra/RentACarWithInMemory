523054ca4c96c0ddaccd26f8df94f039e7ddfdd8
example <haticezehraorhan@gmail.com>
2023-01-19 20:25:26 +0300

    Entity

---
+++ Entities/Concretes/Car.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Car :IEntity
+    {
+        int Id { get; set; }
+        int BrandId { get; set; }
+        int ColorId { get; set; }   
+        int ModelYear { get; set; }
+        int DailyPrice { get; set; }
+        string Description { get; set; }
+
+        
+    }
+}
